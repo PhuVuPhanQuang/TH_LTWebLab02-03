4a4269584ff9102ccb0b84ff21eebcb2d1f7c1a9
Ryder_Martinez <vuphanquangphu2322@gmail.com>
2021-06-20 01:47:52 +0700

    Add project files.

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TH_LTWebLab02_03.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TH_LTWebLab02_03.Startup))]
+namespace TH_LTWebLab02_03
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
